388d9da896318564b35518d838e97a82eb5c9003
Sivli Kestanous <cmtoups@me.com>
2019-12-06 00:02:12 -0800

    make the pink hotter

--- content.js
+++ content.js
@@ -3,18 +3,18 @@ chrome.storage.local.get(defaultOptions, function(settings) {
     let bodyMark = new Mark(document.querySelectorAll('.monitor_body_container .post_body .block')[0])
     , tagsMark = new Mark(document.querySelectorAll('.monitor_body_container .post_tags')[0])
     for (let word of settings.red) {
-      bodyMark.markRegExp(new RegExp('^'+word, "i"), {className: 'red'})
-      tagsMark.markRegExp(new RegExp('^#'+word, "i"), {className: 'red'})
+      bodyMark.markRegExp(new RegExp('^'+word, "i"), {className: 'Red'})
+      tagsMark.markRegExp(new RegExp('^#'+word, "i"), {className: 'Red'})
     }
     for (let word of settings.pink) {
       let rWord = new RegExp('^'+word, "i")
-      bodyMark.markRegExp(new RegExp('^'+word, "i"), {className: 'pink'})
-      tagsMark.markRegExp(new RegExp('^#'+word, "i"), {className: 'pink'})
+      bodyMark.markRegExp(new RegExp('^'+word, "i"), {className: 'HotPink'})
+      tagsMark.markRegExp(new RegExp('^#'+word, "i"), {className: 'HotPink'})
     }
     for (let word of settings.orange) {
       let rWord = new RegExp('^'+word, "i")
-      bodyMark.markRegExp(new RegExp('^'+word, "i"), {className: 'orange'})
-      tagsMark.markRegExp(new RegExp('^#'+word, "i"), {className: 'orange'})
+      bodyMark.markRegExp(new RegExp('^'+word, "i"), {className: 'Orange'})
+      tagsMark.markRegExp(new RegExp('^#'+word, "i"), {className: 'Orange'})
     }
     console.log('nT Monitoring tick');
   }, settings.seconds * 1000);

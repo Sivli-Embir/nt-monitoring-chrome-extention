6e96677f726d43587fabd279dd13bbcb254c59c0
Sivli Kestanous <cmtoups@me.com>
2019-12-06 18:05:09 -0800

    cleaned up options code

--- options.js
+++ options.js
@@ -1,38 +1,36 @@
+const COLORS = ['red', 'pink', 'orange', 'green']
+
 function save_options() {
-  let red = document.getElementById('red').value.split(/\r?\n/)
-  , pink = document.getElementById('pink').value.split(/\r?\n/)
-  , orange = document.getElementById('orange').value.split(/\r?\n/)
-  , green = document.getElementById('green').value.split(/\r?\n/)
-  , seconds = parseFloat(document.getElementById('seconds').value);
-  chrome.storage.local.set({ red, pink, orange, green, seconds }, function() {
-    // Update status to let user know options were saved.
+  let toStorage = {
+    seconds: parseFloat(document.getElementById('seconds').value)
+  }
+  for (let color of COLORS) {
+    toStorage[color] = document.getElementById(color).value.split(/\r?\n/)
+  }
+  chrome.storage.local.set(toStorage, function() {
     let status = document.getElementById('status');
     status.textContent = 'Options saved.';
-    setTimeout(function() {
-      status.textContent = '';
-    }, 750);
+    setTimeout(function() { status.textContent = ''; }, 750);
+  });
+}
+
+function fetch_options() {
+  chrome.storage.local.get(defaultOptions, function(items) {
+    document.getElementById('seconds').value = +items.seconds;
+    for (let color of COLORS) {
+      document.getElementById(color).value = items[color].join('\n');
+    }
   });
 }
 
 function restore_options() {
-  chrome.storage.local.get(defaultOptions, function(items) {
-    document.getElementById('red').value = items.red.join('\n');
-    document.getElementById('pink').value = items.pink.join('\n');
-    document.getElementById('orange').value = items.orange.join('\n');
-    document.getElementById('green').value = items.green.join('\n');
-    document.getElementById('seconds').value = +items.seconds;
-  });
-}
-
-function reset_options() {
-  document.getElementById('red').value = defaultOptions.red.join('\n');
-  document.getElementById('pink').value = defaultOptions.pink.join('\n');
-  document.getElementById('orange').value = defaultOptions.orange.join('\n');
-  document.getElementById('green').value = defaultOptions.green.join('\n');
   document.getElementById('seconds').value = +defaultOptions.seconds;
+  for (let color of COLORS) {
+    document.getElementById(color).value = defaultOptions[color].join('\n');
+  }
   save_options()
 }
 
-document.addEventListener('DOMContentLoaded', restore_options);
+document.addEventListener('DOMContentLoaded', fetch_options);
 document.getElementById('save').addEventListener('click', save_options);
-document.getElementById('restore').addEventListener('click', reset_options);
+document.getElementById('restore').addEventListener('click', restore_options);
